27f8d4a1d8b232c5bf60a76b07ad08532d0fbdf8
PavelSoldatov <akelloghbdtn17177>
2021-07-26 21:15:13 +0300

    -Add logs

--- ariss-barrier-impl/src/main/java/ru/unit/techno/ariss/barrier/resource/BarrierResource.java
+++ ariss-barrier-impl/src/main/java/ru/unit/techno/ariss/barrier/resource/BarrierResource.java
@@ -1,5 +1,6 @@
 package ru.unit.techno.ariss.barrier.resource;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,16 +9,21 @@ import ru.unit.techno.ariss.barrier.api.dto.BarrierRequestDto;
 import ru.unit.techno.ariss.barrier.api.dto.BarrierResponseDto;
 import ru.unit.techno.ariss.barrier.api.enums.BarrierResponseStatus;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/barrier")
 public class BarrierResource {
 
     @PostMapping("/open")
     public BarrierResponseDto openBarrier(@RequestBody BarrierRequestDto request) {
-        System.out.println(request);
+        log.info("Received request to barrier open, request body: {}", request);
 
-        return new BarrierResponseDto()
+        //TODO логика открытия шлагбаума
+        //....какая то логика....
+        BarrierResponseDto barrierResponseDto = new BarrierResponseDto()
                 .setBarrierId(request.getBarrierId())
                 .setBarrierResponseStatus(BarrierResponseStatus.SUCCESS);
+        log.info("Barrier successfully opened, barrier ID {}", request.getBarrierId());
+        return barrierResponseDto;
     }
 }
