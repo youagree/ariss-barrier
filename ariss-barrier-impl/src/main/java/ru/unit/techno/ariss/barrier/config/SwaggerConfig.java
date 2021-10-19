5fdcb9b7ff22f2392c2a540132c059ddcedf1717
Васин Алексей Евгеньевич <empty>
2021-10-19 22:19:01 +0300

    fix swagger params parse

--- ariss-barrier-impl/src/main/java/ru/unit/techno/ariss/barrier/config/SwaggerConfig.java
+++ ariss-barrier-impl/src/main/java/ru/unit/techno/ariss/barrier/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package ru.unit.techno.ariss.barrier.config;
 
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
+import com.fasterxml.classmate.TypeResolver;
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.TypeNameExtractor;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,8 +19,14 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("ru.unit.techno.ariss.barrier"))
+                .apis(Predicates.or(basePackage("ru.unit.techno.ariss.barrier"), basePackage("ru.unit.techno.ariss.log.action.lib")))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public SwaggerOperationParameterReader swaggerOperationParameterReader(TypeNameExtractor nameExtractor,
+                                                                           TypeResolver resolver) {
+        return new SwaggerOperationParameterReader(nameExtractor, resolver);
     }
 }
