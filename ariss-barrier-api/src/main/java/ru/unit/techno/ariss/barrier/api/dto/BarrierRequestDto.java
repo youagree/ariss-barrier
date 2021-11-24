93786ba81910fbd657d4cd5dc7b667221a7a9efe
PavelSoldatov <akelloghbdtn17177>
2021-11-24 22:53:24 +0300

    -Return address field

--- ariss-barrier-api/src/main/java/ru/unit/techno/ariss/barrier/api/dto/BarrierRequestDto.java
+++ ariss-barrier-api/src/main/java/ru/unit/techno/ariss/barrier/api/dto/BarrierRequestDto.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Pattern;
 
 @Data
 public class BarrierRequestDto {
+    private String barrierCoreAddress;
     private Long barrierId;
     @Pattern(regexp = "(^[АВЕКМНОРСТУХ]\\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{3}(?<!000)\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{4}(?<!0000)\\d{2,3})|(^\\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2}\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{3}(?<!000)[АВЕКМНОРСТУХ]\\d{2,3})$")
     private String governmentNumber;
